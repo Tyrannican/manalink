8da653f57e2ef04f03736974faf2fa8756c07399
Graham Keenan <graham.keenan@outlook.com>
2020-06-17 11:24:18 +0100

    Renamed package from xenon to rhenium

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='xenon',
+    name='rhenium',
     version='0.0.1',
     author='Graham Keenan',
     author_email='graham.keenan@outlook.com',
