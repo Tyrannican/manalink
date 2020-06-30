77dc993c48d1b4f3ee04a3232c64d895cc5d281f
Graham Keenan <graham.keenan@outlook.com>
2020-06-30 17:31:34 +0100

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rhenium',
-    version='0.0.1',
+    version='0.1.0',
     author='Graham Keenan',
     author_email='graham.keenan@outlook.com',
     description='Expandable P2P communication protocols',
