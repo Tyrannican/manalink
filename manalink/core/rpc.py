2ae7b56cacedd7cbac1c83bbb736e6ae8edb558d
Graham Keenan <graham.keenan@outlook.com>
2020-12-08 11:21:57 +0000

    Allow user to pass in message buffer size and removed incorrect arguments to ManaGemFinder

--- manalink/core/rpc.py
+++ manalink/core/rpc.py
@@ -60,25 +60,29 @@ class ManaGem:
     have the results sent back when ready.
 
     Args:
-        host (Optional[str]): Host IP address.
+        host (str): Host IP address.
         Defaults to all interfaces "0.0.0.0".
 
-        port: (Optional[int]): Host port to listen on.
+        port: (int): Host port to listen on.
         Defaults to cst.DEFAULT_PORT.
 
         seed_nodes (Optional[List[str]]): Initial seed nodes to help find other
         nodes on the network.
         Required, else the node will sit isolated. Defaults to [].
 
-        debug_mode (Optional[bool]): Debug flag. Defaults to False.
+        debug_mode (bool): Debug flag. Defaults to False.
+
+        message_buffer (int): Size of the message buffer. Defaults to
+        cst.DEFAULT_BUFFER
     """
 
     def __init__(
         self,
-        host: Optional[str] = "0.0.0.0",
-        port: Optional[int] = cst.DEFAULT_PORT,
+        host: str = "0.0.0.0",
+        port: int = cst.DEFAULT_PORT,
         seed_nodes: Optional[List[str]] = [],
-        debug_mode: Optional[bool] = LIB_DEBUG
+        debug_mode: bool = LIB_DEBUG,
+        message_buffer: int = cst.DEFAULT_BUFFER
     ):
         # Current address and port
         self.address = Address(host=host, port=port)
@@ -86,15 +90,14 @@ class ManaGem:
         # Initial nodes to seed P2P
         self.gems = seed_nodes
 
-        # Input buffer
-        self.__buf = cst.DEFAULT_BUFFER
+        # Input buffer for receiving messages
+        self.__buf = message_buffer
 
         # Initiate logger
         self.logger = make_logger(self.name, debug=debug_mode)
 
         self.finder = ManaGemFinder(
             self.address.host,
-            port=cst.DISCOVERY,
             seed_gems=self.gems,
             logger=get_logger(self.name)
         )
@@ -125,6 +128,10 @@ class ManaGem:
             self._server_handler, self.address.host, self.address.port
         )
 
+        self.logger.debug(
+            f"Starting {self.name} protocol server for\
+ {self.address.host}:{self.address.port}"
+        )
         await server.serve_forever()
 
     async def _server_handler(
@@ -144,6 +151,7 @@ class ManaGem:
         # Replaces the 0.0.0.0 with the host's actual IP
         sock = writer.get_extra_info("socket")
         self.address.host = sock.getsockname()[0]
+        self.logger.debug(f"Received connection from {sock.getpeername()[0]}")
 
         # Read request and parse for a response
         request = await reader.read(self.__buf)
@@ -152,6 +160,7 @@ class ManaGem:
         # Return response back to sender
         writer.write(response.encode("utf-8"))
         await writer.drain()
+        self.logger.debug(f"Sent response back to {sock.getpeername()[0]}")
 
         # Close down writer
         writer.close()
@@ -244,7 +253,16 @@ class ManaGemFinder:
         self.__buf = cst.DEFAULT_BUFFER
 
         # Async lock for updating nodes
-        self._gem_lock = asyncio.Lock()
+        self._update_gem_lock = asyncio.Lock()
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the protocol
+
+        Returns:
+            str: Nmae of the protocol
+        """
+        return self.__class__.__name__
 
     async def run(self):
         """Main execution loop for the finder protocol.
@@ -259,7 +277,7 @@ class ManaGemFinder:
         ]
 
         # Execute all tasks
-        self.logger.info("Initialised ManaGemFinder protocol!")
+        self.logger.info(f"Initialised {self.name} protocol.")
         await asyncio.gather(*tasks)
 
     async def _server(self):
@@ -272,6 +290,7 @@ class ManaGemFinder:
             self.address.host, self.address.port
         )
 
+        self.logger.debug(f"Starting {self.name} protocol server.")
         await server.serve_forever()
 
     async def _finder_handler(
@@ -406,7 +425,7 @@ class ManaGemFinder:
         while True:
             # Remove the host node form the list, only care about others
             if self.address.host in self.gems:
-                async with self._gem_lock:
+                async with self._update_gem_lock:
                     self.gems.remove(self.address.host)
 
             # Ping each node we know of
@@ -416,7 +435,7 @@ class ManaGemFinder:
                     continue
 
                 # Node is dead, remove it
-                async with self._gem_lock:
+                async with self._update_gem_lock:
                     self.logger.debug(f"Removing ManaGem: {gem}")
                     self.gems.remove(gem)
 
@@ -436,13 +455,14 @@ class ManaGemFinder:
 
         # If node isn't in the list and is alive, add it
         if gem not in self.gems and await ping(gem, port=self.address.port):
-            async with self._gem_lock:
+            async with self._update_gem_lock:
                 self.logger.debug(f"Registed new ManaGem: {gem}")
                 self.gems.append(gem)
                 self.gems = list(set(self.gems))
 
 async def main():
-    pass
+    m = ManaGem()
+    await m.finder.run()
 
 if __name__ == "__main__":
     asyncio.run(main())
