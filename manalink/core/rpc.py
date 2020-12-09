131f8b8372939fa2be4d6f144175fc0e7804d63a
Graham Keenan <graham.keenan@outlook.com>
2020-12-09 15:29:59 +0000

    Attempting to figure out how to actually execute methods effectively for child protocols

--- manalink/core/rpc.py
+++ manalink/core/rpc.py
@@ -27,9 +27,6 @@ from jsonrpc.jsonrpc2 import (
     JSONRPC20Response
 )
 
-# Debug flag
-LIB_DEBUG = True
-
 class ManaGem:
     """Core of the P2P protocol.
     Exposes methods for facilitating communication between nodes using JSON RPC.
@@ -58,7 +55,7 @@ class ManaGem:
         host: str = "0.0.0.0",
         port: int = cst.DEFAULT_PORT,
         seed_nodes: Optional[List[str]] = [],
-        debug_mode: bool = LIB_DEBUG,
+        debug_mode: bool = False,
         message_buffer: int = cst.DEFAULT_BUFFER
     ):
         # Current address and port
@@ -73,7 +70,8 @@ class ManaGem:
         # Initiate logger
         self.logger = make_logger(self.name, debug=debug_mode)
 
-        self.finder = ManaGemLinker(
+        # GemLinker Protocol (Discovery)
+        self.gemlinker = ManaGemLinker(
             self.address.host,
             seed_gems=self.gems,
             logger=get_logger(self.name)
@@ -93,7 +91,12 @@ class ManaGem:
         """Main run loop.
         """
 
-        pass
+        tasks = [
+            self.server(),
+            self.gemlinker.run()
+        ]
+
+        asyncio.gather(*tasks)
 
     async def server(self):
         """Main server loop for receiving connections from other nodes.
