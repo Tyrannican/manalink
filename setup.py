718aba6a982f4a2e1ad9c7aa087cc0f9bf5d70c8
Graham Keenan <graham.keenan@outlook.com>
2020-12-11 11:32:08 +0000

    Added requirements parsing to setup file and version bump

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as fd:
+    dependencies = [dep for dep in fd.readlines()]
+
 setup(
     name='manalink',
-    version='0.2.3',
+    version='0.3.0',
     author='Graham Keenan',
     author_email='graham.keenan@outlook.com',
     description='Expandable P2P communication protocols',
+    install_requires=dependencies,
     packages=find_packages(),
     python_requires='>=3.7'
 )
